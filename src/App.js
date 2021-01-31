5e61394aa6fb7b103e05a823dea82b58ac0505f4
Avas Bajracharya <insanebuzz09@gmail.com>
2021-01-31 18:11:44 +0545

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import { useTransition, animated } from "react-spring";
 function App() {
   const location = useLocation();
   const transitions = useTransition(location, (location) => location.pathname, {
-    from: { opacity: 0, transform: "translate3d(0,100%,0)" },
-    enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
-    leave: { opacity: 0, transform: "translate3d(0,-100%,0)" },
+    from: { opacity: 0, transform: "translateY(100%)" },
+    enter: { opacity: 1, transform: "translateY(0%)" },
+    leave: { opacity: 0, transform: "translateY(-100%)" },
   });
 
   const animation = transitions.map(({ item: location, props, key }) => (
